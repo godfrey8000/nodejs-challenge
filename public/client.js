7be93a6b5e9d977f382cce7aa64e35e8622869af
Glitch (shaded-potent-canary) <none>
2023-05-26 19:30:05 +0000

    *️⃣🌹 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -11,5 +11,33 @@ const santaForm = document.forms[0];
 // listen for the form to be submitted and add a new dream when it is
 santaForm.onsubmit = function (event) {
   // TODO: check the text isn't more than 100chars before submitting
-  // event.preventDefault();
+  const inputText = santaForm.elements['inputText'].value;
+  
+  if (inputText.length > 100) {
+    event.preventDefault();
+    alert('Oops! Input text should not exceed 100 characters.');
+  }
+  
+  fetch('/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ inputText })
+  })
+  .then(response => {
+    
+    if (response.ok) {
+      console.log('Form submitted successfully');
+      // Perform any desired action on successful form submission
+    } else {
+      console.error('Form submission failed');
+    }
+  })
+  .catch(error => {
+    console.error('Error on submiting form:', error);
+  });
+  
 };
+
+
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 
 app.use(bodyParser());
 app.use(morgan());
-
+app.use(axios());
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -20,6 +22,43 @@ app.use(express.static('public'));
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
+
+
+
+
+// adding endpoint for recieving santa form submit
+app.post('/submit', (request, response) => {
+  const inputText = request.body.inputText;
+  const userProfiles = "";
+  const users = "";
+  
+    // Make a GET request to retrieve the userProfiles.json data
+  axios.get('https://raw.githubusercontent.com/alj-devops/santa-data/master/userProfiles.json')
+    .then(response => {
+      userProfiles = response.data;
+      console.log(userProfiles);
+    })
+    .catch(error => {
+      console.error('Error retrieving userProfiles.json:', error);
+    });
+
+  // Make a GET request to retrieve the users.json data
+  axios.get('https://raw.githubusercontent.com/alj-devops/santa-data/master/users.json')
+    .then(response => {
+      users = response.data;
+      console.log(users);
+    })
+    .catch(error => {
+      console.error('Error retrieving users.json:', error);
+    });
+  
+  
+
+  response.sendStatus(200);
+});
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function () {
